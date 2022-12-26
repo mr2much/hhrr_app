5df3fd2503ee48ada97f206d3b1d087a2e298e74
Anubis Lockward <anubis.lockward@gmail.com>
2022-12-26 12:23:21 -0400

    Add leaflet and setup basic map

---
+++ public/map/js/map.js
@@ -0,0 +1,7 @@
+const map = L.map('map', { center: [0, 0], zoom: 2 });
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
