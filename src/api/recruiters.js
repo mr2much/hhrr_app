2150d67e267720ec735e817e201a96aaa18c63df
Anubis Lockward <anubis.lockward@gmail.com>
2025-01-08 07:16:12 -0400

    Add login and register pages

--- src/api/recruiters.js
+++ src/api/recruiters.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const title = 'AristoCrew';
+
 router.get('/register', (req, res) => {
-  res.render('recruiters/register', { title: 'AristoCrew' });
+  res.render('recruiters/register', { title });
+});
+
+router.get('/login', (req, res) => {
+  res.render('recruiters/login', { title });
 });
 
 module.exports = router;
