6139513a35eb56ade946b82b9329da9e7db658cc
Anubis Lockward <anubis.lockward@gmail.com>
2025-01-11 08:06:50 -0400

    Bug fix and register route completion
    - Autofill was causing a bug in Chrome
    - Finish register route for recruiter

--- src/api/routes/recruiters.js
+++ src/api/routes/recruiters.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const Recruiter = require('../db/models/recruiters/recruiter');
 const router = express.Router();
+const passport = require('passport');
 
 const title = 'AristoCrew';
 
@@ -7,8 +9,29 @@ router.get('/register', (req, res) => {
   res.render('recruiters/register', { title });
 });
 
-router.post('/register', (req, res) => {
-  res.send(req.body);
+router.post('/register', async (req, res) => {
+  try {
+    const { recruiter } = req.body;
+    const { email, username, password } = recruiter;
+
+    const newRecruiter = new Recruiter({ email, username });
+    const registeredRecruiter = await Recruiter.register(
+      newRecruiter,
+      password
+    );
+
+    req.login(registeredRecruiter, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      req.flash('success', 'Welcome to AristoCrew');
+      res.redirect('/api/v1/candidatos/');
+    });
+  } catch (e) {
+    req.flash('error', e.message);
+    res.redirect('/api/v1/recruiter/login');
+  }
 });
 
 router.get('/login', (req, res) => {
